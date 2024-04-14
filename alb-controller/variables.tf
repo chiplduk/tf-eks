4125e5a7868efa548a87b87cd75028d97d2dd5fa
Vsevolod <sertum01@gmail.com>
2024-04-14 14:56:12 +0200

    Removed extra modules

--- alb-controller/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "alb_controller_namespace" {
-  description = "Namespace for ALB controller"
-  default     = "kube-system"
-}
-
-variable "bucket_name" {
-  description = "Bucket name to get data from remote state"
-  type = string
-}
--- argocd/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "alb_name" {
-  description = "Name of Ingress ALB"
-  default     = "dev-eks-ingress"
-}
-
-variable "bucket_name" {
-  description = "Bucket name to get data from remote state"
-  type = string
-}
